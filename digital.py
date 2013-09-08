a77a403ba5e20d6b0278aaef9d8a4d095bb21ca9
Brian Engert <lart2150@gmail.com>
2013-09-08 13:55:31 -0500

    changing to work with a pi vs the usb to gpi that I was using before.  I also switched to pull down as they help with the switch bouncing.

--- digital.py
+++ digital.py
@@ -1,5 +1,4 @@
 import sys
-import serial
 import time
 import re
 import httplib
@@ -7,47 +6,45 @@ import pygame
 import ConfigParser
 import json
 import subprocess
+import RPi.GPIO as GPIO
 from datetime import datetime
 
 config = ConfigParser.ConfigParser()
 config.read('alarm.cfg')
 
-portName = config.get('Settings', 'portName')
 
 #Open port for communication	
-serPort = serial.Serial(portName, 115200, timeout=1)
 
+pygame.init();
 pygame.init();
 pygame.mixer.music.load(config.get('Settings', 'doorChime'));
 
+#GPIO.setmode(GPIO.BOARD);
+GPIO.setmode(GPIO.BCM)
 
-
-def getState(serPort, gpioNum): 
-	serPort.write("gpio read "+ str(gpioNum) + "\n\r");
-	response = serPort.read(18);
-	response = response.split("\n\r")[1];
+def getState(gpioNum): 
+	response = str(GPIO.input(gpioNum));
 	return response;
 	
 def alert(gpioNum, newstate):
-	conn = httplib.HTTPConnection(config.get('Settings', 'httpHost'))
-	conn.request("GET", config.get('Settings', 'httpURI') + "/doorPort/" + str(gpioNum) + "/eventType/" + newstate)
-	r1 = conn.getresponse()
-	jsonDecoded = json.loads(r1.read())
-	return jsonDecoded
+	conn = httplib.HTTPConnection(config.get('Settings', 'httpHost'));
+	conn.request("GET", config.get('Settings', 'httpURI') + "/doorPort/" + str(gpioNum) + "/eventType/" + newstate);
+	r1 = conn.getresponse();
+	jsonDecoded = json.loads(r1.read());
+	return jsonDecoded;
 	
-def monitorStateChange(serPort, gpioNum, newstate):
+def monitorStateChange(gpioNum, newstate):
 	start = datetime.now();
 	now = datetime.now();
 	delta = now - start;
 	monitorTime = 100000;
-	if (gpioNum == 3):
-		monitorTime = 500000;
+	time.sleep(.1);
 	while (delta.microseconds < monitorTime) :
-		tmpState = getState(serPort, gpioNum)
+		tmpState = getState(gpioNum);
 		if (tmpState != newstate):
 			now = datetime.now();
 			delta = now - start;
-			print "bailed at " + str(delta.microseconds) + " of " + str(monitorTime) + " port: " + str(gpioNum) + " started: " + start.isoformat(' ');
+			print "bailed at " + str(delta.microseconds) + "  port: " + str(gpioNum) + " started: " + start.isoformat(' ');
 			return False;
 		now = datetime.now();
 		delta = now - start;
@@ -56,17 +53,19 @@ def monitorStateChange(serPort, gpioNum, newstate):
 states = eval(config.get('Settings', 'sensors'));
 shellcmd = config.get('Settings', 'shell');
 
+for (i, state) in enumerate(states):
+	GPIO.setup(state[0], GPIO.IN);
+
 while True:
 	for (i, state) in enumerate(states):
-		newstate = getState(serPort, state[0]);
+		newstate = getState(state[0]);
 		if (newstate != state[1]):
-			if (monitorStateChange(serPort, state[0], newstate)):
+			if (monitorStateChange(state[0], newstate)):
 				print "State Changed to: " + newstate + " Port: " + str(state[0]) + " at: " + datetime.now().isoformat(' ');
-				if (newstate == '1'):
+				if (newstate == '0'):
 					pygame.mixer.music.play()
 				responce = alert( state[0], newstate);
-				args = ['/home/lart/alarm/event.sh', str(responce['eventID']), responce['armedStatus'], newstate]
-				print args
-				p = subprocess.Popen(args)
+#				args = ['/home/lart/alarm/event.sh', str(responce['eventID']), responce['armedStatus'], newstate]
+#				p = subprocess.Popen(args)
 				states[i][1] = newstate;
-serPort.close()
+#	time.sleep(.1);
